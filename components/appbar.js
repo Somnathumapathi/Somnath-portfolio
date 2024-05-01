6629868a0b849b6e99a69793af25aff5a794b92b
Somnathumapathi <somnathumapathi7@gmail.com>
2024-05-02 00:49:34 +0530

    first commit

---
+++ components/appbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link'
+import {SiGithub, SiLinkedin, SiInstagram} from 'react-icons/si';
+
+const AppBar = () => {
+    const social = [
+        {
+            link : "https://www.linkedin.com/in/somnath-umapathi-9a485a205/",
+            icon : SiLinkedin,
+        },
+        {
+            link : "https://github.com/Somnathumapathi",
+            icon : SiGithub,
+        },
+        {
+            link : "https://www.instagram.com/somnath_umapathi/",
+            icon : SiInstagram,
+        }
+    ]
+    return (
+        <nav className='py-10 flex justify-between items-center'>            
+            <h1 className='text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2'>Somnath U 👨🏾‍💻</h1>             
+            <div className='flex items-center gap-5'>
+                {social.map((s,i)=>{
+                    const Icon = s.icon
+                    return <Link href={s.link} key={i}>
+                        <Icon className='w-5 h-5 hover:scale-125 transition-all hover:text-orange-500'/>
+                    </Link>
+                })}
+            </div>
+        </nav>
+    )
+}
+
+export default AppBar
---
+++ components/projects.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+function Projects() {
+
+const projects = [
+    {
+        title: "Crave Hub",
+        link: "https://github.com/Somnathumapathi/CraveHub",
+        description: "Health conscious food ordering website.",
+        techstack: ["NextJS", "Firebase"]
+    },
+    {
+        title: "ExpressMart",
+        description: "E-commerce app",
+        link: "https://github.com/Somnathumapathi/ExpressMart",
+        techstack: ["Flutter", "ExpressJs", "MongoDb"]
+    },
+    {
+        title: "Status 200",
+        description: "Project related question answer platform",
+        link: "https://github.com/Somnathumapathi/Status200",
+        techstack: ["Flutter", "Firebase"]
+    },
+    {
+        title: "Worcse",
+        description: "Website for demand creator and supply provider contracts",
+        link: "https://github.com/KoushikMCN/Worcse",
+        techstack: ["NextJS", "Firebase", "Solidity"]
+    },
+    {
+        title: "chEthFunds",
+        description: "Chitfunds using blockchain",
+        link: "https://github.com/synapsecode/chETHFunds",
+        techstack: ["NextJS", "Firebase", "Solidity"]
+    },
+    {
+        title: "TrashTrace",
+        description: "Complete Waste managemant solution.",
+        link: "https://github.com/synapsecode/TrashTrace-MiniProject",
+        techstack: ["Flutter", "Flask"]
+    },
+    {
+        title: "RedFrontier",
+        description: "Mars colonization helpmate",
+        link: "https://github.com/synapsecode/RedFrontier",
+        techstack: ["Flutter", "Firebase"]
+    },
+]
+
+  return (
+    <div className="max-w-screen-lg mx-auto">
+             <div  className='flex flex-col items-center justify-center -rotate-2 pb-10'><h1 className='text-2xl font-semibold hover:text-green-500'>Projects 🚀</h1>
+                    <div className='pr-2 w-40 h-1 bg-green-500 rounded-full'></div><div className='pl-2 w-40 h-1 bg-blue-500 translate-x-2 rounded-full'></div></div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {projects.map((project, index) => (
+                    <div key={index} className="shadow-md border border-gray-200 rounded-lg overflow-hidden hover:bg-blue-950">
+                        <div className="p-4">
+                            <h2 className="text-xl font-semibold mb-2">{project.title}</h2>
+                            <p className="text-gray-500 mb-4">{project.description}</p>
+                            <div className="flex flex-wrap">
+                                {project.techstack.map((tech, index) => (
+                                    <span key={index} className="bg-grey-500 rounded-full px-2 py-1 text-sm font-semibold text-green-500 mr-2 mb-2">{tech}</span>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="p-4 bg-gray-800">
+                            <a href={project.link} className="text-blue-500 hover:underline">View on GitHub</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+  )
+}
+
+export default Projects
