a1e1f48f324d30fabe5b6d15521eef8c029e1856
Somnathumapathi <somnathumapathi7@gmail.com>
2024-10-06 17:08:29 +0530

    new portfolio work

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
+// 'use client'
 import AppBar from "@/components/appbar";
 import Experience from "@/components/experience";
 import MainSection from "@/components/mainsection";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="min-h-[400vh] bg-gradient-to-br from-black to-blue-950">
+      <Link href={'/Home1'}><div>New portfolio under construction ⚒️. Click here to see.</div></Link>
       <div className="max-w-7xl mx-auto p-10">
         <AppBar/>
         <MainSection/>
       </div>
+
+      
       <div className="max-w-7xl mx-auto p-10">
         <Skills/>
       </div>
